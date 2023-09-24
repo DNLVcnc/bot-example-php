55ee6e04986a5f7f6e5e89910d002c529bd65c4b
DNLVcnc <143163629+DNLVcnc@users.noreply.github.com>
2023-09-24 10:48:19 +0500

    Update config.php

--- www/config.php
+++ www/config.php
@@ -6,5 +6,5 @@ define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
 define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
 
 define('CALLBACK_API_CONFIRMATION_TOKEN', 'bb112c41'); //Строка для подтверждения адреса сервера из настроек Callback API
-define('VK_API_ACCESS_TOKEN', '016cb55230eb83e34f620389s149345d975bc12e2fb6f30e0a833a51d345d0d123c3dc0abc1c864036sdf989fb8345'); //Ключ доступа сообщества
+define('VK_API_ACCESS_TOKEN', '016cb55230eb83e34f620389s149345d975bc12e2fb6f30e0a833a51d345d0d123c3dc0abc1c864036sdf989fb8345'); //vk1.a.vG_dwkE1mPgZjDxigpjogL5mHEx09qC1z4zEvUhwg7Qq9Dy6SU-YqJ8Q0J_dd0RuwSPbvtH10XDw2eb3ntYE9A5jeY2nKOPpcl96FPg1UByHjP3ZavoH0B1k0Ejja5XmcTV28mLhdMUzeWGVKyj9OkaD2-z85nNpHUPBtL_8RTQ88B6rvo4npcloaTwbg1VPRpDtoWrL_DW2-qf8vfaKWw
 define('YANDEX_API_KEY', '30e3213440-61233-1294-b3415-471212369886'); //Ключ для доступа к Yandex Speech Kit
